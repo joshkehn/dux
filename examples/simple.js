81381828faed95cfb04ea69b6164a966fa58c05d
Joshua Kehn <josh.kehn@gmail.com>
2011-12-19 11:13:46 -0500

    Better command exec

--- examples/simple.js
+++ examples/simple.js
@@ -2,10 +2,14 @@ var dux = require('../lib/dux');
 
 dux.config({
     'name' : {
-        default : 'Josh'
+        default : '[no name]'.red
     },
     'debug' : {
         alias : 'd',
+        boolean : true,
+        default : false
+    },
+    'short' : {
         boolean : true,
         default : false
     }
@@ -15,12 +19,21 @@ if (dux.options.debug) {
     dux.logger.info('Debugging ' + 'enabled'.green);
     dux.logger.set_levels('all');
 }
+dux.logger.short(dux.options.short);
 
-dux.default('run');
-dux.commands.add('run', function () {
-    if (dux.options.name) {
-        dux.logger.info('Hello ' + dux.options.name.green);
-    }
+dux.default('hello');
+
+dux.commands.add('hello', function () {
+    dux.logger.info('Hello ' + dux.options.name.green);
+});
+
+dux.commands.add('goodbye', function () {
+    dux.logger.info('Goodbye ' + dux.options.name.green);
+});
+
+dux.commands.add('hellogoodbye', function () {
+    dux.commands.run('hello');
+    dux.commands.run('goodbye');
 });
 
 dux.start();
